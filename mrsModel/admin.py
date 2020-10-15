5060d08238a1a725309f09c8079dee0de110512c
Marcos <marcos.blandon@unitedlifts.com.au>
2020-10-16 08:17:50 +1100

    Initial commit, refactoring after generation from tables

---
+++ mrsModel/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
+
+def registerModels(*app_list):
+    for app_name in app_list:
+        app_models = apps.get_models(app_name)
+        for model in app_models:
+            try:
+                admin.site.register(model)
+            except AlreadyRegistered:
+                pass
+
+registerModels('mrsModel')
