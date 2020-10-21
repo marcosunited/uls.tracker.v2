3d48aee23a6faa890159446f5cc9fe1687c42917
Marcos <marcos.blandon@unitedlifts.com.au>
2020-10-21 12:49:02 +1100

    1.0.0.7
    move user view to class based
    add cache system to auth related entities

--- mrsauth/urls.py
+++ mrsauth/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.urls import include
 from rest_framework import routers
 
-from mrsauth.views import user, login
+from mrsauth.views import UserViewSet, login
 from mrsauth.views.AuthorizationViewSets import GroupViewSet, PermissionViewSet, ProfileViewSet
 
 router = routers.DefaultRouter()
@@ -15,10 +15,10 @@ urlpatterns = [
     url(r'^api/v1/login$', login.do_login),
 
     # User urls
-    url(r'^api/v1/user/create$', user.user_init),
-    url(r'^api/v1/user$', user.user_list),
-    url(r'^api/v1/user/(?P<pk>[0-9]+)$', user.user_detail),
-    url(r'^api/v1/user/filter$', user.user_filter),
+    url(r'^api/v1/user/create$', UserViewSet.UserInit.as_view()),
+    url(r'^api/v1/user$', UserViewSet.UserList.as_view()),
+    url(r'^api/v1/user/(?P<pk>[0-9]+)$', UserViewSet.UserDetail.as_view()),
+    url(r'^api/v1/user/filter$', UserViewSet.UserFilter.as_view()),
 
     # Authentication authorization router managed urls
     url(r'^api/v1/', include(router.urls)),
