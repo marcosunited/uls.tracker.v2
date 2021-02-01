48f33e054085b5a73cdf98f7994a65de34d3fda1
Marcos <71992699+marcosunited@users.noreply.github.com>
2021-02-01 13:16:10 +1100

    Update BusinessViewSets.py

--- mrs/views/BusinessViewSets.py
+++ mrs/views/BusinessViewSets.py
@@ -227,28 +227,14 @@ class NoteViewSet(FilteredModelViewSet):
     serializer_class = NotesSerializer
 
 
-class ProcedureViewSet(FilteredModelViewSet):
-    queryset = Procedure.objects.all()
-    serializer_class = ProceduresSerializer
-
-
-class OperationViewSet(FilteredModelViewSet):
-    queryset = Operation.objects.all()
-    serializer_class = OperationsSerializer
-
-
-class ActionViewSet(FilteredModelViewSet):
-    queryset = Action.objects.all()
-    serializer_class = ActionsSerializer
-
-
+"""
 # /procedures/idProcedure/tasks/idTask/
-class ProcedureTaskRelationView(APIView):
-    def post(self, request, pk_procedure, pk_task):
+class MaintenanceMonthTaskTemplateRelationView(APIView):
+    def post(self, request, pk_maintenance_month, pk_task_template):
         try:
-            procedure = Procedure.objects.get(id=pk_procedure)
-            task = Task.objects.get(id=pk_task)
-            procedure.tasks.add(task)
+            maintenance_month = MaintenanceMonth.objects.get(id=pk_maintenance_month)
+            task_template = TaskTemplate.objects.get(id=pk_task_template)
+            maintenance_month.task_templates.add(task_template)
             procedure_serializer = ProceduresSerializer(procedure)
             return JsonResponse({'result': procedure_serializer.data, 'error': ''})
         except Procedure.DoesNotExist:
@@ -271,16 +257,23 @@ class ProcedureTaskRelationView(APIView):
             return JsonResponse(ResponseHttp(error='The task does not exist').result, status=HTTP_404_NOT_FOUND)
         except Exception as error:
             return JsonResponse(ResponseHttp(error=str(error)).result, status=HTTP_500_INTERNAL_SERVER_ERROR)
+"""
 
 
-class TaskViewSet(FilteredModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class = TasksSerializer
+class TaskTemplateViewSet(FilteredModelViewSet):
+    queryset = TaskTemplate.objects.all()
+    serializer_class = TaskTemplateSerializer
 
 
-class MaintenancePlanViewSet(FilteredModelViewSet):
-    queryset = MaintenancePlan.objects.all()
-    serializer_class = MaintenancePlansSerializer
+class MaintenanceMonthViewSet(FilteredModelViewSet):
+    queryset = MaintenanceMonth.objects.all()
+    serializer_class = MaintenanceMonthSerializer
+
+
+class YearMaintenanceTemplateViewSet(FilteredModelViewSet):
+    queryset = YearMaintenanceTemplate.objects.all()
+    serializer_class = YearMaintenanceTemplateSerializer
+
 
 
 class GenerateMaintenancePlanView(APIView):
@@ -299,8 +292,3 @@ class GenerateMaintenancePlanView(APIView):
             return JsonResponse({'result': pk_lift, 'error': ''})
         except Exception as error:
             return JsonResponse(ResponseHttp(error=str(error)).result, status=HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-class ScheduleEntryViewSet(FilteredModelViewSet):
-    queryset = ScheduleEntry.objects.all()
-    serializer_class = ScheduleEntriesSerializer
